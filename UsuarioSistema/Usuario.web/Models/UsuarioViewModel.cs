569a68ce12de29959e1f47311a3e3ddbc2ad5921
francisco.acosta <francisco.acosta@linktic.com>
2025-06-13 15:05:10 -0500

    add views

---
+++ UsuarioSistema/Usuario.web/Models/UsuarioViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Usuario.Web.Models
+{
+    public class UsuarioViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Nombre { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        public string? Sexo { get; set; }
+    }
+}
---
+++ UsuarioSistema/Usuario.web/Views/Usuario/Gestion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Usuario.web.Views.Usuario
+{
+    public class GestionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
